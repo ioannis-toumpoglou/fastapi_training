0f57dff1f543154385dba478d0b928a668748fb5
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-04-27 16:05:18 +0300

    Validate book data using Pydantic

--- books2.py
+++ books2.py
@@ -1,5 +1,6 @@
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 app = FastAPI()
@@ -24,11 +25,11 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 BOOKS = [
@@ -53,4 +54,9 @@ async def get_all_books():
 async def create_book(book_request: BookRequest):
     # Convert request to Book object
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
