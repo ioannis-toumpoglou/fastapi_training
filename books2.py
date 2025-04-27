02093058baf8bab6f32a202db4d69dccd3bcfc9b
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-04-28 01:44:13 +0300

    Implement validation for path parameters

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -65,7 +65,7 @@ async def get_all_books():
     return BOOKS
 
 @app.get("/books/{book_id}")
-async def get_book(book_id: int):
+async def get_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -109,7 +109,7 @@ async def update_book(book: BookRequest):
 
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
