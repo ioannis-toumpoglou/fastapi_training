fef292a979921c560061ba0f3b006aa13abd0d93
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-04-27 17:07:45 +0300

    Pydantic configuration

--- books2.py
+++ books2.py
@@ -25,11 +25,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="Not required", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "Coding with Roby",
+                "description": "A new book description",
+                "rating": 5,
+            }
+        }
+    }
 
 
 BOOKS = [
