d86d5075aa716b291b1e01be013f34daf99aa6ae
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-04-28 01:43:10 +0300

    Implement the GET book by date published method

--- books2.py
+++ books2.py
@@ -15,13 +15,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -30,6 +32,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1900, lt=2031)
 
     model_config = {
         "json_schema_extra": {
@@ -38,18 +41,19 @@ class BookRequest(BaseModel):
                 "author": "Coding with Roby",
                 "description": "A new book description",
                 "rating": 5,
+                "published_date": 2025
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, "Computer Science Pro", "Coding with Roby", "Great CS book", 5),
-    Book(2, "Be Fast with FastAPI", "Coding with Roby", "Great book", 5),
-    Book(3, "Master Endpoints", "Coding with Roby", "Good CS book", 5),
-    Book(4, "HP1", "Author 1", "An average book", 2),
-    Book(5, "HP2", "Author 2", "A nice book", 3),
-    Book(6, "HP3", "Author 3", "A terrible book", 1)
+    Book(1, "Computer Science Pro", "Coding with Roby", "Great CS book", 5, 2023),
+    Book(2, "Be Fast with FastAPI", "Coding with Roby", "Great book", 5, 2024),
+    Book(3, "Master Endpoints", "Coding with Roby", "Good CS book", 5, 2025),
+    Book(4, "HP1", "Author 1", "An average book", 2, 2020),
+    Book(5, "HP2", "Author 2", "A nice book", 3, 2021),
+    Book(6, "HP3", "Author 3", "A terrible book", 1, 2022)
 ]
 
 
@@ -82,6 +86,15 @@ async def create_book(book_request: BookRequest):
     # Convert request to Book object
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
+
+
+@app.get("/books/publish/")
+async def get_book_by_published_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
 
 def find_book_id(book: Book):
